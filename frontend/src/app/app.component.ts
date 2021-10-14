58da956d5c7a38d55efac2c5c0fbe699b21d1abf
Timm Ortloff <me@tortloff.de>
2021-10-14 22:19:45 +0200

    Added Minimize and Maximize methods

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
+// FIXME: Not working with Wails beta yet
+declare var window: any;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'frontend';
+
+  close(): void {
+    window.runtime.Quit();
+  }
+
+  minimize(): void {
+    window.runtime.WindowMinimise();
+  }
 }
+
